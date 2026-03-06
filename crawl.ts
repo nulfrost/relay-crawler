c94564877099034271f65866d9bb3b4cf81f9778
besaid.zone <me@dane.computer>
2026-03-05 21:31:18 -0500

    add discord webhook reporting

--- crawl.ts
+++ crawl.ts
@@ -1,25 +1,31 @@
 import type { Host } from "./types.ts";
 
-const response = await fetch(`${Deno.env.get("RELAY_HOST")}/admin/pds/list`, {
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: `Basic ${
-      btoa("admin:" + Deno.env.get("RELAY_ADMIN_PASSWORD"))
-    }`,
+const pdsListResponse = await fetch(
+  `${Deno.env.get("RELAY_HOST")}/admin/pds/list`,
+  {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Basic ${
+        btoa("admin:" + Deno.env.get("RELAY_ADMIN_PASSWORD"))
+      }`,
+    },
   },
-});
+);
 
-if (!response.ok) {
+if (!pdsListResponse.ok) {
   throw new Error(
-    `there was an error fetching the pds list: ${response.status}`,
+    `there was an error fetching the pds list: ${pdsListResponse.status}`,
   );
 }
 
-const hosts = await response.json() as Host[];
+const hosts = await pdsListResponse.json() as Host[];
 
 const disconnectedHosts = hosts.filter((host) =>
   host.HasActiveConnection === false
 );
+
+let successfulConnections = 0;
+let unsuccessfulConnections = 0;
 
 for (const host of disconnectedHosts) {
   const response = await fetch(
@@ -40,8 +46,41 @@ for (const host of disconnectedHosts) {
         (data as Error).message
       }`,
     );
+    unsuccessfulConnections += 1;
     continue;
   }
 
   console.info(`[INFO] crawled ${host.Host} at ${new Date().toISOString()}`);
+  successfulConnections += 1;
+}
+
+// summary of this run sent to discord webhook
+const DISCORD_WEBHOOK_TOKEN = Deno.env.get("DISCORD_WEBHOOK_TOKEN");
+const response = await fetch(
+  `https://discord.com/api/webhooks/1479298793735196716/${DISCORD_WEBHOOK_TOKEN}`,
+  {
+    method: "POST",
+    body: JSON.stringify({
+      embeds: [
+        {
+          "title": "Relay Re-crawl",
+          "description": "Summary of recrawl github action",
+          timestamp: new Date().toISOString(),
+          "fields": [
+            {
+              "name": "added",
+              "value": `${successfulConnections} PDSes successfully re-crawled`,
+            },
+            {
+              "name": "failures",
+              "value": `${unsuccessfulConnections} PDSes failed to re-crawl`,
+            },
+          ],
+        },
+      ],
+    }),
+  },
+);
+if (!response.ok) {
+  throw new Error(`failed to send message to webhook: ${response.status}`);
 }
