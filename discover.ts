10e3593924ed50ad2a1478614774da7669008d5a
besaid.zone <me@dane.computer>
2026-03-06 08:33:23 -0500

    add webhook to discovery action

--- discover.ts
+++ discover.ts
@@ -69,6 +69,9 @@ const upstreamRelayHosts = new Set(
 
 const newHosts = upstreamRelayHosts.difference(currentHosts);
 
+let successfulAdditions = 0;
+let unsuccessfulAdditions = 0;
+
 for (const host of Array.from(newHosts)) {
   const response = await fetch(
     `${Deno.env.get("RELAY_HOST")}/admin/pds/requestCrawl`,
@@ -91,8 +94,46 @@ for (const host of Array.from(newHosts)) {
         (data as Error).message
       }`,
     );
+    unsuccessfulAdditions += 1
     continue;
   }
 
+  successfulAdditions +=1
   console.info(`[INFO]: added ${host} to relay`);
+}
+
+// summary of this run sent to discord webhook
+const DISCORD_WEBHOOK_TOKEN = Deno.env.get("DISCORD_WEBHOOK_TOKEN");
+const webhookResponse = await fetch(
+  `https://discord.com/api/webhooks/1479298793735196716/${DISCORD_WEBHOOK_TOKEN}`,
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      embeds: [
+        {
+          "title": "New PDS additions",
+          "description": "Summary of discovery github action",
+          timestamp: new Date().toISOString(),
+          "fields": [
+            {
+              "name": "added",
+              "value": `${successfulAdditions} PDSes successfully added`,
+            },
+            {
+              "name": "failures",
+              "value": `${unsuccessfulAdditions} PDSes did not get added`,
+            },
+          ],
+        },
+      ],
+    }),
+  },
+);
+if (!webhookResponse.ok) {
+  const data = await response.json();
+  console.log({ data });
+  throw new Error(`failed to send message to webhook: ${webhookResponse.status}`);
 }
